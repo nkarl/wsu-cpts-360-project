3dfc1d1ff65932a55c31934d850352b6a7c50e66
nkarl <21993921+nkarl@users.noreply.github.com>
2023-04-17 12:38:22 -0700

    Started working on mkdir.

---
+++ hdr/creat_mkdir.h
@@ -0,0 +1,9 @@
+#ifndef CREAT_MKDIR_H
+#define CREAT_MKDIR_H
+
+#include "../hdr/type.h"
+#include <stdlib.h>
+
+void my_mkdir(char *);
+
+#endif
---
+++ src/creat_mkdir.c
@@ -0,0 +1,12 @@
+#include "../hdr/creat_mkdir.h"
+#include "../hdr/util.h"
+#include <time.h>
+
+extern PROC *running;
+extern int   dev;
+extern int   fd, dev;
+
+extern MINODE *root;
+
+void my_mkdir(char *pathname) {
+}
