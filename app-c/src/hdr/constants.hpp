0c9b65ed91133190cacc96de6a93f25eda0c5d55
nkarl <21993921+nkarl@users.noreply.github.com>
2023-12-16 16:43:42 -0800

    Changed test from printf to assert.

---
+++ app-c/src/hdr/constants.hpp
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace constants {
+    constexpr unsigned short BLOCK_SIZE = 1024;
+}
+
+#endif
--- app-c/src/utils.cc
+++ app-c/src/utils.cc
@@ -1,15 +1,71 @@
 #include "hdr/add.hpp"
+#include "hdr/constants.hpp"
+#include "hdr/ext2fs.hpp"
 #include "hdr/sub.hpp"
+#include <cassert>
+#include <ctime>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void boot_test() {
-    printf("%s", "\nTest -- Compiling and Linking Objects: ");
-    int a = add(2, 5);
-    printf("add 2 5 == %d, ", a);
-    int b = sub(5, 2);
-    printf("sub 5 2 == %d\n\n", b);
-}
+typedef struct ext2_super_block SUPER;
 
-void print(char *s, unsigned int x) {
-    printf("%-30s = %8d\n", s, x);
+struct VDisk {
+    int    fd;
+    int    blksize = constants::BLOCK_SIZE;
+    int    inodesize;
+    SUPER *sp;
+    char   buf[constants::BLOCK_SIZE];
+
+    void readSUPER(char *device) {
+        fd = open(device, O_RDONLY);
+        if (fd < 0) {
+            printf("open %sfailed\n", device);
+            exit(1);
+        }
+        lseek(fd, (long)1024 * 1, 0);  // block 1 on FD, offset 1024 on HD
+        read(fd, buf, 1024);
+        sp = (SUPER *)buf;
+        // as a super block structure
+        // check EXT2 FS magic number:
+        printf("%-30s = %8x ", "s_magic", sp->s_magic);
+        if (sp->s_magic != 0xEF53) {
+            printf("NOT an EXT2 FS\n");
+            exit(2);
+        }
+        printf("EXT2 FS OK\n");
+        print("s_inodes_count",
+              sp->s_inodes_count);
+        print("s_blocks_count",
+              sp->s_blocks_count);
+        print("s_r_blocks_count",
+              sp->s_r_blocks_count);
+        print("s_free_inodes_count", sp->s_free_inodes_count);
+        print("s_free_blocks_count", sp->s_free_blocks_count);
+        print("s_first_data_block", sp->s_first_data_block);
+        print("s_log_block_size",
+              sp->s_log_block_size);
+        print("s_blocks_per_group", sp->s_blocks_per_group);
+        print("s_inodes_per_group", sp->s_inodes_per_group);
+        print("s_mnt_count",
+              sp->s_mnt_count);
+        print("s_max_mnt_count",
+              sp->s_max_mnt_count);
+        printf("%-30s = %8x\n", "s_magic", sp->s_magic);
+        printf("s_mtime = %s", std::ctime((long *)&sp->s_mtime));
+        printf("s_wtime = %s", std::ctime((long *)&sp->s_wtime));
+        blksize = 1024 * (1 << sp->s_log_block_size);
+        printf("block size = %d\n", blksize);
+        printf("inode size = %d\n", sp->s_inode_size);
+    }
+
+    void print(char const *s, unsigned int x) {
+        printf("%-30s = %8d\n", s, x);
+    }
+};
+
+void test_boot_compile_link() {
+    assert((void("\nTest Add: "), add(2, 5) == 7));
+    assert((void("\nTest Sub: "), sub(5, 2) == 3));
 }
