5b3de8a93cff97d041a26c1e83fa3ce7f20cedee
nkarl <21993921+nkarl@users.noreply.github.com>
2023-12-20 15:19:09 -0800

    Scaffolded a test suite for validating EXT2 and possibly an EXT2 fs.

---
+++ app-c/src/tests/ext2-validation.cc
@@ -0,0 +1,33 @@
+#include "../hdr-locked/my-types.hpp"
+#include "../hdr-locked/my-fs.hpp"
+
+#include <cassert>
+
+void test_ext2_super_magic_number() {
+    /*
+     * 1. mock the FS::EXT2 struct
+     * 2. read the super_block and assert on the magic_number
+     */
+    assert((void("\nsuper magic_number"), 0 == 1));
+}
+
+void test_ext2_super_log_block_number() {
+    /*
+     * 1. mock the FS::EXT2 struct
+     * 2. read the super_block and assert on the log_block_number
+     */
+    assert((void("\nsuper log_block_number"), 0 == 1));
+}
+
+void test_ext2_fs() {
+    /*
+     * 1. send string as script to bash
+     *   - creates a new disk dd
+     * 2. open and read from the created disk
+     *
+     * 3. test on the struct mapped from the created disk
+     *
+     */
+    assert((void("\nsuper magic_number"), 0 == 1));
+    assert((void("\nsuper log_block_number"), 0 == 1));
+}
